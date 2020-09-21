bfb96770f3b9f6c1f4bab855e27de0c0f977560c
julio-toma <julio_toma@yahoo.com.br>
2020-09-20 23:51:05 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 49):
+    def __init__(self, *filhos, nome = None, idade = 49):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Alice')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Julio'
-    print(p.nome)
-    print(p.idade)
+    julio = Pessoa(nome ='Julio')
+    alice = Pessoa(julio, nome ='Alice')
+    print(Pessoa.cumprimentar(alice))
+    print(id(alice))
+    print(alice.cumprimentar())
+    print(alice.nome)
+    print(alice.idade)
+    for filho in alice.filhos:
+        print(filho.nome)
